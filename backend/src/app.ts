88129f838e98410abef30ff24ad02689afb57b61
ninealways <navnit_dav@yahoo.co.in>
2023-09-06 15:07:27 +0530

    added routes & controller with get & post methods

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,18 +1,17 @@
 import "dotenv/config";
 import express, { NextFunction, Request, Response } from "express";
-import NoteModel from "./models/note";
+import notesRoute from './routes/notes';
+import morgan from 'morgan';
+
 const app = express();
 
-app.get('/', async (req, res, next) => {
-    try {
-        const notes = await NoteModel.find().exec();
-        res.status(200).json(notes);
-    } catch (error) {
-        next(error);
-    }
+app.use(morgan("dev")); //this is just to log api end point and status code in console.
 
-});
+app.use(express.json());
 
+app.use("/api/notes", notesRoute); //defines the actual route to be used for all api's
+
+//this handles for any endpoints that does not exist in our application
 app.use((req, res, next) => {
     next(Error("Endpoint not found!"));
 });
@@ -24,4 +23,5 @@ app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
     if (error instanceof Error) errorMessage = error.message;
     res.status(500).json({ error: errorMessage });
 });
+
 export default app;
