7b98a2f8ba5d3b86d247e1559bd0523dd17c9b36
ninealways <navnit_dav@yahoo.co.in>
2023-09-11 12:15:28 +0530

    add notes added to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,27 +2,56 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import { Note as NoteModel } from './models/note';
 import Note from './Components/Note';
+import * as NotesApi from './Network/notes.api';
 
 function App() {
   const [notes, setNotes] = useState<NoteModel[]>([]);
+  const [note, setNote] = useState({
+    title: "",
+    text: ""
+  });
+
+  async function loadNotes() {
+    try {
+      const notes = await NotesApi.fetchNotes();
+      setNotes(notes);
+      return notes;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name;
+    const val = e.target.value;
+    setNote({ ...note, [name]: val });
+  }
+
+  const addNote = async() => {
+    try {
+      const addednote = await NotesApi.createNote(note);
+      console.log(addednote);
+      setNote({
+        title: "",
+        text: ""
+      });
+      await loadNotes();
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   useEffect(() => {
-    async function loadNotes() {
-      try {
-        const response = await fetch('/api/notes', { method: 'GET' });
-        const notes = await response.json();
-        console.log(notes);
-        setNotes(notes);
-        return notes;
-      } catch (error) {
-        console.log(error);
-      }
-    }
     loadNotes();
   }, []);
 
   return (
     <div className="app">
+      <div className='add-note'>
+        <input className='add-title' type="text" value={note.title} placeholder="add title" name="title" onChange={(e) => handleInputChange(e)} />
+        <input className='add-text' type="text" value={note.text} placeholder="add text (optional)" name="text" onChange={(e) => handleInputChange(e)} />
+        <button type='submit' className='add-submit' onClick={addNote}>Add Note </button>
+      </div>
       <div className="notes">
         {notes.map(note => {
           return (
