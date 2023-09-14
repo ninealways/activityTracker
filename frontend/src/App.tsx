01e908d44ffc942840f5c108fcc6625368fb3ae5
ninealways <navnit_dav@yahoo.co.in>
2023-09-14 12:42:10 +0530

    F: login added to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,8 +3,32 @@ import './App.css';
 import { Note as NoteModel } from './models/note';
 import Note from './Components/Note';
 import * as NotesApi from './Network/notes.api';
+import { User } from './models/user';
 
 function App() {
+  const [loggedInUser, setLoggedInUser] = useState<User | null>(null);
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  });
+  const handleUserInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name;
+    const val = e.target.value;
+    setUser({ ...user, [name]: val });
+  }
+
+  const loginClick = async () => {
+    const response = await NotesApi.login(user);
+    setLoggedInUser(response);
+    console.log(response);
+  }
+
+  const logoutClick = async () => {
+    const response = await NotesApi.logout();
+    setLoggedInUser(null);
+    console.log(response);
+  }
+
   const [notes, setNotes] = useState<NoteModel[]>([]);
   const [note, setNote] = useState({
     title: "",
@@ -78,29 +102,50 @@ function App() {
     setEditNoteId('');
   }
   useEffect(() => {
+    async function fetchLoggedInUser() {
+      try {
+        const user = await NotesApi.getLoggedInUser();
+        console.log(user);
+        setLoggedInUser(user);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchLoggedInUser();
     loadNotes();
   }, []);
 
   return (
     <div className="app">
-      <div className='add-note'>
-        <input className='add-title' type="text" value={note.title} placeholder="add title" name="title" onChange={(e) => handleInputChange(e)} />
-        <input className='add-text' type="text" value={note.text} placeholder="add text (optional)" name="text" onChange={(e) => handleInputChange(e)} />
-        <button type='submit' className='add-submit' onClick={editNoteId !== '' ? updateNote : addNote}>{editNoteId !== '' ? "Update Note" : "Add Note"}</button>
-        {editNoteId !== '' ? <button className='cancel-button' onClick={cancelEdit}>Cancel</button> : ""}
-      </div>
-      <div className="notes">
-        {notes.map(note => {
-          return (
-            <Note
-              note={note}
-              onDeleteNoteClicked={deleteNote}
-              onEditNoteClicked={editNote}
-              key={note._id}
-            />
-          )
-        })}
-      </div>
+      {!loggedInUser ?
+        <div className="login-form">
+          <input className='inputs username' type="email" value={user.email} placeholder="Please enter email" name="email" onChange={(e) => handleUserInputChange(e)} />
+          <input className='inputs password' type="password" value={user.password} placeholder="Please enter password" name="password" onChange={(e) => handleUserInputChange(e)} />
+          <button type='submit' className='submit login-submit' onClick={loginClick}>Login</button>
+        </div>
+        :
+        <>
+          <button type="submit" className='submit logout-submit' onClick={logoutClick}>Logout</button>
+          <div className="add-note">
+            <input className='inputs add-title' type="text" value={note.title} placeholder="add title" name="title" onChange={(e) => handleInputChange(e)} />
+            <input className='inputs add-text' type="text" value={note.text} placeholder="add text (optional)" name="text" onChange={(e) => handleInputChange(e)} />
+            <button type='submit' className='submit add-submit' onClick={editNoteId !== '' ? updateNote : addNote}>{editNoteId !== '' ? "Update Note" : "Add Note"}</button>
+            {editNoteId !== '' ? <button className='cancel-button' onClick={cancelEdit}>Cancel</button> : ""}
+          </div>
+          <div className="notes">
+            {notes.map(note => {
+              return (
+                <Note
+                  note={note}
+                  onDeleteNoteClicked={deleteNote}
+                  onEditNoteClicked={editNote}
+                  key={note._id}
+                />
+              )
+            })}
+          </div>
+        </>
+      }
     </div>
   );
 }
--- frontend/src/Network/notes.api.ts
+++ frontend/src/Network/notes.api.ts
@@ -1,4 +1,5 @@
 import { Note } from "../models/note";
+import { User } from "../models/user";
 
 async function fetchData(input: RequestInfo, init?: RequestInit) {
     const response = await fetch(input, init);
@@ -11,6 +12,48 @@ async function fetchData(input: RequestInfo, init?: RequestInit) {
         throw Error(errorMessage);
     }
 };
+
+export async function getLoggedInUser(): Promise<User> {
+    const response = await fetchData("api/users", { method: "GET" });
+    return response.json();
+}
+
+export interface SignUpCred {
+    username: string,
+    email: string,
+    password: string,
+}
+
+export async function signUp(cred: SignUpCred): Promise<User> {
+    const response = await fetchData("api/users/signup", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(cred)
+    });
+    return response.json();
+}
+
+export interface LoginCred {
+    email: string,
+    password: string,
+}
+
+export async function login(cred: LoginCred): Promise<User> {
+    const response = await fetchData("api/users/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(cred)
+    });
+    return response.json();
+}
+
+export async function logout() {
+    await fetchData("api/users/logout", { method: "POST" });
+}
 
 export async function fetchNotes(): Promise<Note[]> {
     const response = await fetchData('/api/notes', { method: 'GET' });
